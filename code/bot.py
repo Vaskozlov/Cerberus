1fe727629ed23d56db89ef5617a1f6785ed3dd49
Vasiliy <vaskozlov3940@gmail.com>
2022-03-21 21:43:46 +0500

    Update bot.py

--- code/bot.py
+++ code/bot.py
@@ -140,5 +140,5 @@ def send_text(message):
     except BaseException:
         return
 
-bot.infinity_polling((1 <<23), (1 << 23))
+bot.infinity_polling((1 << 16), (1 << 16))
 print("palling ended")
