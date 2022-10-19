92079ec03b25b0714357be91d514088a623dacb7
maxGrigorenko <50823330+maxGrigorenko@users.noreply.github.com>
2022-10-19 20:21:14 +0500

    Update adminFunctionality.py

--- code/adminFunctionality.py
+++ code/adminFunctionality.py
@@ -135,10 +135,10 @@ def get_fish_info(self):
 
     try:
         usr = clientController.get_user_from_login(info[1])
-        if usr.clogin == usr.login and usr.cpassword == usr.password:
+        if usr.cerberusLogin == usr.login and usr.cerberusPassword == usr.password:
             data_plus = ''
         else:
-            data_plus = f'clogin: {usr.clogin}, cpassword: {usr.cpassword}'
+            data_plus = f'clogin: {usr.cerberusLogin}, cpassword: {usr.cerberusPassword} '
             
         data = f"name: {usr.name}, login: {usr.login}, {data_plus}password: {usr.password}, paid_answers: {usr.paid_answers}, registration_time: {usr.registration_time}"
         bot.send_message(self.chat_id, data)
